18ece0ecb26d8b7a63adaf214ac089e94cead920
zudhhyy <zudhy.duddy@gmail.com>
2020-06-26 14:13:09 +0700

    user model done

---
+++ server/user-newsFeed/helpers/jwt.js
@@ -0,0 +1,20 @@
+const jwt = require("jsonwebtoken");
+const secretKey = process.env.secretKey;
+// const secretKey = 'inirahasia'
+
+function generateToken(payload) {
+  let access_token = jwt.sign(
+    {
+      id: payload.id,
+      email: payload.email,
+    },
+    secretKey
+  );
+  return access_token;
+}
+
+function verifyToken(access_token) {
+  return jwt.verify(access_token, secretKey);
+}
+
+module.exports = { generateToken, verifyToken };
