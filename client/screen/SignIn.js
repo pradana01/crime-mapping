9ce51871e529f4985eac2036e21841937cb8f68f
chabode <rizqa.aulia@gmail.com>
2020-06-26 22:54:13 +0700

    Client Development Progress + create Home screen with newsfeed component + create form crime report on Create tab + create My Report tab with Report component + create account screen + create sign in and sign up screen with tab + create Comment screen with params navigation - adding authentication for token / async store. ini authenticationnya masih sebatas true/false - adding redux for storing data fetch from server - adding google map api for crime map screen - editing css in client

---
+++ client/screen/SignIn.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import { StyleSheet, TextInput, View, Text, Button, Dimensions } from 'react-native';
+
+
+export default function SignIn() {
+  return (
+    <View style={styles.container}>
+      <Text>Crime Port</Text>
+      <View>
+        <Text>Sign In</Text>
+        <View style={styles.column}>
+          <Text>Username</Text>
+          <TextInput style={styles.input} placeholder="username"/>
+        </View>
+        <View style={styles.column}>
+          <Text>Password</Text>
+          <TextInput style={styles.input} placeholder="password" secureTextEntry={true}/>
+        </View>
+        <View style={styles.column}>
+          <Button title="Sign In"/>
+          <Text>If you are new, Register Here!</Text>
+        </View>
+      </View>
+      <View style={styles.column}>
+          <Text>Or Login With Google</Text>
+          <Button title="Google"/>
+        </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    
+  },
+  column: {
+    width: Dimensions.get('window').width-100,
+    margin: 2
+  },
+  input : {
+    borderColor: "black",
+    borderWidth: 1,
+  }
+});
