b1a30aeab9b9a39bf20855368f1f900cf8204d11
zudhhyy <zudhy.duddy@gmail.com>
2020-06-27 12:46:27 +0700

    done test

--- server/mapDataBase/test/mapData.test.js
+++ server/mapDataBase/test/mapData.test.js
@@ -1,23 +1,70 @@
-const app = require('../app-test.js')
-const { District, sequelize } = require('../models')
-const request = require('supertest')
-const { queryInterface } = sequelize
+const app = require("../app-test.js");
+const { sequelize, District } = require("../models");
+const request = require("supertest");
+const { queryInterface } = sequelize;
+
+const createDistrict = {
+  name: "cempaka putih",
+  city: "jakarta pusat",
+  homicide: 1,
+  assault: 1,
+  harassment: 1,
+  abduction: 1,
+  robbery: 1,
+  theft: 1,
+  drugs: 1,
+  fraudulency: 1,
+  anarchism: 1,
+};
+
+beforeAll((done) => {
+  District.create(createDistrict)
+    .then(() => done())
+    .catch((err) => {
+      done(err);
+    });
+});
 
 afterAll((done) => {
-    done()
-})
+  queryInterface
+    .bulkDelete("Districts", {})
+    .then(() => done())
+    .catch((err) => {
+      done(err);
+    });
+});
 
-describe('Districts route test', () => {
-    describe('GET /districts - get all districts crime data', () => {
-        test('200 Success get all data - should return data', () => {
-            request(app)
-            .get('/districts')
-            .then((response) => {
-                const { body, status } = response
-                expect(status).toBe(201);
-                expect(body).toBeTypeOf('array')
-                done()
-            })
-        })
-    })
-})
+describe("Districts route test", () => {
+  describe("GET /districts - get all districts crime data", () => {
+    test("Should return 200 status & an array containing objects of crime data", function (done) {
+      request(app)
+        .get("/districts")
+        .then((response) => {
+          const { body, status } = response;
+          expect(status).toBe(200);
+          expect(body).toEqual(expect.arrayContaining([expect.objectContaining(createDistrict)]));
+          //   expect(body).toEqual(expect.arrayContaining([expect.any(Object)]));
+          done();
+        });
+    });
+  });
+
+  describe("GET /districts/:id - get districts crime data by id", () => {
+    test("Should return 200 status & an array containing objects of crime data", function (done) {
+      request(app)
+        .get("/districts")
+        .then((response) => {
+          request(app)
+            .get(`/districts/${response.body[0].id}`)
+            .then((response2) => {
+              const { body, status } = response;
+              expect(status).toBe(200);
+              expect(body).toEqual(
+                expect.arrayContaining([expect.objectContaining(createDistrict)])
+              );
+              done();
+            });
+        });
+    });
+  });
+});
