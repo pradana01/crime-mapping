2fdea520db3161f9e6f6138d9be25ea3f42875ec
zudhhyy <zudhy.duddy@gmail.com>
2020-06-28 00:22:48 +0700

    add maps, polygon, geotag

--- client/App.js
+++ client/App.js
@@ -1,46 +1,46 @@
-import React from 'react';
-import { NavigationContainer } from '@react-navigation/native'
-import { createStackNavigator } from '@react-navigation/stack'
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
-import Home from './screen/Home'
-import Page1 from './screen/Page1'
-import Page2 from './screen/Page2'
-import Page3 from './screen/Page3'
-import Page4 from './screen/Page4'
-import Page5 from './screen/Page5'
-import SignIn from './screen/SignIn'
-import SignUp from './screen/SignUp'
+import Home from "./screen/Home";
+import Page1 from "./screen/Page1";
+import Page2 from "./screen/Page2";
+import Page3 from "./screen/Page3";
+import Page4 from "./screen/Page4";
+import Page5 from "./screen/Page5";
+import SignIn from "./screen/SignIn";
+import SignUp from "./screen/SignUp";
 
-const Stack = createStackNavigator()
-const Tab = createBottomTabNavigator()
+const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
 
 const HomeNavigator = () => {
   return (
-  <Stack.Navigator>
-    <Stack.Screen name='Newsfeed Report' component={Home} />
-    <Stack.Screen name='Comment' component={Page5} />
-  </Stack.Navigator>
-  )
-}
+    <Stack.Navigator>
+      <Stack.Screen name="Newsfeed Report" component={Home} />
+      <Stack.Screen name="Comment" component={Page5} />
+    </Stack.Navigator>
+  );
+};
 
 export default function App() {
-  const isLogin = true
+  const isLogin = true;
   return (
     <NavigationContainer>
       <Tab.Navigator>
         {isLogin ? (
           <>
-          <Tab.Screen name="Home" component={HomeNavigator}/>
-          <Tab.Screen name="Crime Map" component={Page1}/>
-          <Tab.Screen name="Create" component={Page2}/>
-          <Tab.Screen name="My Report" component={Page3}/>
-          <Tab.Screen name="Account" component={Page4}/>
+            <Tab.Screen name="Home" component={HomeNavigator} />
+            <Tab.Screen name="Crime Map" component={Page1} />
+            <Tab.Screen name="Create" component={Page2} />
+            <Tab.Screen name="My Report" component={Page3} />
+            <Tab.Screen name="Account" component={Page4} />
           </>
         ) : (
           <>
-          <Tab.Screen name="Sign In" component={SignIn} />
-          <Tab.Screen name="Sign Up" component={SignUp} />
+            <Tab.Screen name="Sign In" component={SignIn} />
+            <Tab.Screen name="Sign Up" component={SignUp} />
           </>
         )}
       </Tab.Navigator>
--- client/screen/Home.js
+++ client/screen/Home.js
@@ -1,41 +1,42 @@
-import React from 'react';
-import { StyleSheet, Text, View, FlatList, ScrollView, Button } from 'react-native';
-import Newsfeed from '../components/Newsfeed'
-import Constants from 'expo-constants'
+import React from "react";
+import { StyleSheet, Text, View, FlatList, ScrollView, Button } from "react-native";
+import Newsfeed from "../components/Newsfeed";
+import Constants from "expo-constants";
 
 const Data = [
-    {
-        id: '1',
-        title: 'AAA',
-        comment: "wah gak bener ini asu",
-        image: 'https://risetcdn.jatimtimes.com/images/2020/02/06/Masuk-2020-600-Lebih-Tindak-Kejahatan-Belum-Diungkap-Polres-Malang73bb576a2f94d5ab.md.jpg',
-    },
-    {
-        id: '2',
-        title: 'BBB',
-        comment: 'ih jambret',
-        image: 'https://cdn2.tstatic.net/jambi/foto/bank/images/ilustrasi-jambret.jpg',
-    },
-    {
-        id: '3',
-        title: 'CCC',
-        comment: 'Maling curanmor',
-        image: 'https://tribratanewsbengkulu.com/wp-content/uploads/20160109011636-curanmor.jpg',
-    }
-]
+  {
+    id: "1",
+    title: "AAA",
+    comment: "wah gak bener ini asu",
+    image:
+      "https://risetcdn.jatimtimes.com/images/2020/02/06/Masuk-2020-600-Lebih-Tindak-Kejahatan-Belum-Diungkap-Polres-Malang73bb576a2f94d5ab.md.jpg",
+  },
+  {
+    id: "2",
+    title: "BBB",
+    comment: "ih jambret",
+    image: "https://cdn2.tstatic.net/jambi/foto/bank/images/ilustrasi-jambret.jpg",
+  },
+  {
+    id: "3",
+    title: "CCC",
+    comment: "Maling curanmor",
+    image: "https://tribratanewsbengkulu.com/wp-content/uploads/20160109011636-curanmor.jpg",
+  },
+];
 
-export default function Home({navigation: {navigate}}) {
-
+export default function Home({ navigation: { navigate } }) {
   return (
     <View style={styles.container}>
       {/* <View style={{marginTop: Constants.statusBarHeight }}>
         <Text style={{fontSize:26}}>Newsfeed report</Text>
       </View> */}
       <View>
-        <FlatList 
+        <FlatList
           data={Data}
           renderItem={(item) => <Newsfeed props={item} />}
-          keyExtractor={item => item.id}/>
+          keyExtractor={(item) => item.id}
+        />
       </View>
     </View>
   );
@@ -44,8 +45,8 @@ export default function Home({navigation: {navigate}}) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
