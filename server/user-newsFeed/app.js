e3723128641604808ccf35e7292cccab4afdf22e
andreanwirapradana <61081321+andreanwirapradana@users.noreply.github.com>
2020-06-29 05:49:26 +0700

    add hosting fiture to server

--- server/user-newsFeed/app.js
+++ server/user-newsFeed/app.js
@@ -3,6 +3,7 @@ const express = require("express");
 const router = require("./routes");
 const cors = require("cors");
 const errorHandler = require("./middlewares/errorHandler");
+const fileupload = require('express-fileupload')
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -10,6 +11,7 @@ const PORT = process.env.PORT || 3001;
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(fileupload({useTempFiles: true}))
 
 app.use(router);
 app.use(errorHandler);
