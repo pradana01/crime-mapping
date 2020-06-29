428392b5ff2d395ad6e55f2c3726cafb0da94256
andreanwirapradana <61081321+andreanwirapradana@users.noreply.github.com>
2020-06-29 17:15:49 +0700

    edit orches and user newsfeed

--- server/orchestrator/app.js
+++ server/orchestrator/app.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const app = express()
 const routes = require('./routers')
+const fileupload = require('express-fileupload')
+
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(fileupload({useTempFiles: true}))
 app.use(routes)
 
 app.listen(3000, () => {
